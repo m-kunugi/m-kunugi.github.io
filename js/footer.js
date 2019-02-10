a9e7a1d84c14ba7c4f2a659208e00118181d55a6
kunugi <kunugimitsu@kunugi-MBA.local>
2019-02-10 19:04:28 +0900

    haya2の軌道追加

--- js/footer.js
+++ js/footer.js
@@ -11,7 +11,7 @@ document.write('\
 				</article>\
 				<article class="grid copyright">\
 					<p><iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fkunugiha.github.io%2F&width=130&layout=button_count&action=like&size=small&show_faces=true&share=true&height=46&appId" width="130" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>\
-					<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Kunugi&#39;s Websiteを訪問しました!" data-url="https://kunugiha.github.io" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p>\
+					<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Kunugi&#39;s Websiteを訪問しました!" data-url="https://m-kunugi.github.io" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p>\
 					Copyright(c) 2018 kunugi All Rights Reserved. Design by <a href="http://f-tpl.com" target="_blank" rel="nofollow">http://f-tpl.com</a>\
 				</article>\
 			</section>\
--- js/header1.js
+++ js/header1.js
@@ -26,7 +26,7 @@ document.write('\
 				<span>趣味のサイト</span></a><br>\
 				<h1></h1>\
 				<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fkunugiha.github.io%2F&width=130&layout=button_count&action=like&size=small&show_faces=true&share=true&height=46&appId" width="130" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>\
-				<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Kunugi&#39;s Websiteを訪問しました!" data-url="https://kunugiha.github.io" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\
+				<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Kunugi&#39;s Websiteを訪問しました!" data-url="https://m-kunugi.github.io" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\
 			</div>\
 			<nav id="mainNav">\
 				<div class="inner">\
