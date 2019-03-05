17ddd6a3dab34d407a1487c8d1fce28beabe86b8
kunugi <kunugimitsu@kunugi-MBA.local>
2019-03-06 01:33:53 +0900

    連絡先追加

--- js/footer.js
+++ js/footer.js
@@ -6,8 +6,8 @@ document.write('\
 					<p class="logo"><a href="index.html" title="ホーム" rel="home">Kunugi\'s Website<br /><span>趣味のサイト</span></a></p>\
 				</article>\
 				<article class="grid">\
-					<p class="tel"><span>電話:</span> <strong>なし</strong></p>\
-					<p class="open">受付時間: 平日 PM 18:00 〜 PM 23:00</p>\
+					<p class="tel"><span>お問い合わせ:</span> <strong>kunugiweb[at]gmail.com</strong> <span>[at]は@に変えてください。</span></p>\
+					<!--<p class="open">受付時間: 平日 PM 18:00 〜 PM 23:00</p>-->\
 				</article>\
 				<article class="grid copyright">\
 					<p><iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fkunugiha.github.io%2F&width=130&layout=button_count&action=like&size=small&show_faces=true&share=true&height=46&appId" width="130" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>\
