8b7d51a9afa5778433b9785f164a9189a4164aa1
m-kunugi <38278310+m-kunugi@users.noreply.github.com>
2019-08-22 04:54:08 +0900

    大幅に組み替え

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,16 @@ document.write('\
 <div class="inner">\
 			<section class="gridWrapper">\
 				<article class="grid">\
-					<p class="logo"><a href="index.html" title="ホーム" rel="home">Kunugi\'s Website<br /><span>趣味のサイト</span></a></p>\
+					<p class="logo"><a href="index.html" title="ホーム" rel="home">Kunugi\'s Website<br />\
+					<span>\
+					<script>\
+						var modified = new Date(document.lastModified);\
+						var year = modified.getFullYear();\
+						var month= modified.getMonth() + 1;\
+						var date = modified.getDate();\
+						document.write( "最終更新 " + year + "/" + month + "/" + date);\
+					</script>\
+					</span></a><br>\
 				</article>\
 				<article class="grid">\
 					<p class="tel"><span>お問い合わせ:</span> <strong>kunugiweb[at]gmail.com</strong> <span>[at]は@に変えてください。</span></p>\
@@ -12,7 +21,7 @@ document.write('\
 				<article class="grid copyright">\
 					<p><iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fkunugiha.github.io%2F&width=130&layout=button_count&action=like&size=small&show_faces=true&share=true&height=46&appId" width="130" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>\
 					<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Kunugi&#39;s Websiteを訪問しました!" data-url="https://m-kunugi.github.io" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p>\
-					Copyright(c) 2018 kunugi All Rights Reserved. Design by <a href="http://f-tpl.com" target="_blank" rel="nofollow">http://f-tpl.com</a>\
+					<span>Copyright(c) 2019 kunugi All Rights Reserved. Design by <a href="http://f-tpl.com" target="_blank" rel="nofollow">http://f-tpl.com</a></span>\
 				</article>\
 			</section>\
 		</div>\
