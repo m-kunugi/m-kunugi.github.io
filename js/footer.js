720d51eb3b1106a7e4610376c98688cf163a83c2
m-kunugi <38278310+m-kunugi@users.noreply.github.com>
2019-08-31 21:34:40 +0900

    修正

--- js/footer.js
+++ js/footer.js
@@ -21,7 +21,12 @@ document.write('\
 				<article class="grid copyright">\
 					<p><iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fkunugiha.github.io%2F&width=130&layout=button_count&action=like&size=small&show_faces=true&share=true&height=46&appId" width="130" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>\
 					<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Kunugi&#39;s Websiteを訪問しました!" data-url="https://m-kunugi.github.io" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p>\
-					<span>Copyright(c) 2019 kunugi All Rights Reserved. Design by <a href="http://f-tpl.com" target="_blank" rel="nofollow">http://f-tpl.com</a></span>\
+					<span>\
+					<script>\
+						var modified = new Date(document.lastModified);\
+						var year = modified.getFullYear();\
+						document.write( "Copyright(c) " + year + " kunugi All Rights Reserved." );\
+					</script> Design by <a href="http://f-tpl.com" target="_blank" rel="nofollow">http://f-tpl.com</a></span>\
 				</article>\
 			</section>\
 		</div>\
